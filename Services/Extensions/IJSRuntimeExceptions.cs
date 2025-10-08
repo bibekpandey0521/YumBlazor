dee22f02ade25879d48f7db32e0a00f41f774911
bibekpandey0521 <bibekpandey276@gmail.com>
2025-10-08 10:04:05 +0545

    Toastr Notifications

---
+++ Services/Extensions/IJSRuntimeExceptions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.JSInterop;
+
+namespace YumBlazor.Services.Extensions
+{
+    public static class IJSRuntimeExceptions
+    {
+        public static async Task ToastrSuccess(this IJSRuntime js,string message)
+        {
+            await js.InvokeVoidAsync("ShowToastr","success", message);
+        }
+        public static async Task ToastError(this IJSRuntime js, string message)
+        {
+            await js.InvokeVoidAsync("ShowToastr", "error" , message);
+        }
+    }
+}
